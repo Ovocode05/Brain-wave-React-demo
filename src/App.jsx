0e297c5fc56aa98ed9d16e6b86fe325fade69b4c
KRRISH PUNJ <142911896+Ovocode05@users.noreply.github.com>
2024-05-06 23:45:58 +0530

    Made a button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import Button from "./components/Button";
+import ButtonGradient from "./assets/svg/ButtonGradient";
 function App() {
-  return <div className="underline text-white">this is me</div>;
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-10">Something</Button>
+      </div>
+      <ButtonGradient />
+    </>
+  );
 }
 
 export default App;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import ButtonSvg from "../assets/svg/ButtonSvg";
 
-function Button() {
-  return <div>button</div>;
+function Button({ children, classname, href, onClick, px, white }) {
+  const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
+    px || "px-7"
+  } ${white ? "text-n-8" : "text-n-1"}`;
+  const spanClases = "relative z-10";
+  const renderButton = () => (
+    <button className={classes}>
+      <span className={spanClases}>{children}</span>
+      {ButtonSvg(white)}
+    </button>
+  );
+  const renderLink = () => (
+    <a href={href}>
+      <span className={spanClases}>{children}</span>
+      {ButtonSvg(white)}
+    </a>
+  );
+  return href ? renderLink() : renderButton();
 }
 
 export default Button;
