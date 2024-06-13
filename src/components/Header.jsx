6337c7e7038cd8f4ab1b9ffeefe8f523adad73f7
KRRISH PUNJ <142911896+Ovocode05@users.noreply.github.com>
2024-06-13 17:37:57 +0530

    header done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,31 +8,41 @@ import { useState } from "react";
 import { enablePageScroll, disablePageScroll } from "scroll-lock";
 
 const Header = () => {
+  // usestates
   const pathname = useLocation();
   const [openNavigation, setopenNavigation] = useState(false);
+
+  // functions
   const toggleNavigation = () =>
     !openNavigation
-      ? (setopenNavigation(true), disablePageScroll())
-      : (setopenNavigation(false), enablePageScroll());
+      ? (() => {
+          setopenNavigation(true), disablePageScroll();
+        })()
+      : (() => {
+          setopenNavigation(false), enablePageScroll();
+        })();
   const handleClick = () => {
     if (!openNavigation) return;
     setopenNavigation(false);
     enablePageScroll();
   };
+
+  //returning back to me
   return (
     <div
-      className={`fixed top-0 left-0 w-full z-50 border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
+      className={` fixed top-0 left-0 w-full z-50 border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
         openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
       }`}
     >
-      <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
+      <div className=" flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a className=" block w-[12rem] mr-8" href="#hero">
           <img src={brainwave} width={190} height={40} alt="Brainwave" />
         </a>
         <nav
           className={`${
             openNavigation ? "flex" : "hidden"
-          } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent"`}
+          } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent `}
+          onClick={handleClick}
         >
           <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
             {navigation.map((item) => (
